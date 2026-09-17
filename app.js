f551722adf5ff581fe21acb40927eb5cfd72dfb6
Ubuntu <ubuntu@ip-10-63-173-238.ap-southeast-2.compute.internal>
2026-09-17 08:54:37 +0000

    App.js modified for unit testing and port changed.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 8080;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+const port = 8000;
+app.get("/", (req, res) => res.send("Hello World!"));
+if (require.main === module) {
+  app.listen(port);
+  console.log(`App running on http://localhost:${port}`);
+}
+module.exports = app;
